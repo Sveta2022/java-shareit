24e2441351b5e161899d4291051bd135ee38d075
Svetlana Yakovleva <lls2007@yandex.ru>
2022-11-10 01:12:03 +0300

    feat: add-bookings

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,14 +5,6 @@ create table if not exists users
     email VARCHAR not null unique
 );
 
-create table if not exists requests
-(
-    id           BIGINT generated always as identity primary key,
-    description  VARCHAR(100),
-    requestor_id BIGINT references users on delete cascade
-
-);
-
 create table if not exists items
 (
     id           BIGINT generated always as identity primary key,
@@ -20,18 +12,6 @@ create table if not exists items
     description  VARCHAR(100) not null,
     owner_id     VARCHAR references users on delete cascade,
     is_available BOOLEAN
-);
-
-create table if not exists items_requests
-(
-    item_id    BIGINT references items,
-    request_id BIGINT references requests
-);
-
-create table if not exists statuses
-(
-    id   INTEGER primary key,
-    name VARCHAR(20)
 );
 
 create table if not exists bookings
@@ -44,14 +24,6 @@ create table if not exists bookings
     status     VARCHAR
 );
 
-create table if not exists requests
-(
-    id           BIGINT generated always as identity primary key,
-    description  VARCHAR(100),
-    requestor_id BIGINT references users on delete cascade
-
-);
-
 create table if not exists comments
 (
     id        BIGINT generated always as identity primary key,
@@ -59,7 +31,6 @@ create table if not exists comments
     item_id   BIGINT references ITEMS on delete cascade,
     author_id BIGINT references USERS on delete cascade,
     created   TIMESTAMP WITHOUT TIME ZONE
-
 );
 
 
